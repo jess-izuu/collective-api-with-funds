8301ff9a207ed20833252d8b1f92e58d5556c656
Hayden Farnsworth <hfmets@gmail.com>
2021-12-21 13:53:09 -0600

    Updated data and fixed routes file

--- app/funds/routes.js
+++ app/funds/routes.js
@@ -1,47 +1,48 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../../lib/db')
+const express = require("express");
+const router = express.Router();
+const db = require("../../lib/db");
 
-router.get('/funds', (req, res) => {
-  const funds = db.funds.findAll().slice(0).sort((a, b) => a.name > b.name ? 1 : 0)
+router.get("/funds", (req, res) => {
+  const funds = db.funds
+    .findAll()
+    .slice(0)
+    .sort((a, b) => (a.name > b.name ? 1 : 0));
   console.log("htting the funds route", funds[0]);
-  res.json(funds)
-})
+  res.json(funds);
+});
 
-router.get('/funds/:id', (req, res) => {
-  const fund = db.funds.find(req.params.id)
-  res.json(fund)
-})
+router.get("/funds/:id", (req, res) => {
+  const fund = db.funds.find(req.params.id);
+  res.json(fund);
+});
 
-router.post('/funds', (req, res) => {
+router.post("/funds", (req, res) => {
   const newFund = db.funds.insert({
-    "title": req.body.title,
-    "subtitle": req.body.subtitle,
-    "author": req.body.author,
-    "published": req.body.published,
-    "publisher": req.body.publisher,
-    "pages": req.body.pages,
-    "description": req.body.description,
-    "website": req.body.website,
-    "inCart": false
-  })
-  res.json(newFund)
-})
+    name: req.body.name,
+    ticker: req.body.ticker,
+    asset_class: req.body.asset_class,
+    risk_level: req.body.risk_level,
+    expense_ratio: req.body.expense_ratio,
+    investment_min: req.body.investment_min,
+    price: req.body.price,
+    change_price: req.body.change_price,
+  });
+  res.json(newFund);
+});
 
-router.put('/funds/:id', (req, res) => {
+router.put("/funds/:id", (req, res) => {
   const updatedFund = db.funds.findByIdAndUpdate(req.params.id, {
-    "title": req.body.title,
-    "subtitle": req.body.subtitle,
-    "author": req.body.author,
-    "published": req.body.published,
-    "publisher": req.body.publisher,
-    "pages": req.body.pages,
-    "description": req.body.description,
-    "website": req.body.website,
-    "inCart": false
-  })
-  res.json(updatedBook)
-})
+    name: req.body.name,
+    ticker: req.body.ticker,
+    asset_class: req.body.asset_class,
+    risk_level: req.body.risk_level,
+    expense_ratio: req.body.expense_ratio,
+    investment_min: req.body.investment_min,
+    price: req.body.price,
+    change_price: req.body.change_price,
+  });
+  res.json(updatedFund);
+});
 
 // router.patch('/books/cart/add/:id', function (req, res) {
 //   const book = db.books.find(req.params.id)
@@ -55,18 +56,15 @@ router.put('/funds/:id', (req, res) => {
 //   res.json(book)
 //})
 
-router.patch('/funds/:id', (req, res) => {
-   const fund = db.funds.findByIdAndUpdate(req.params.id, req.body)
-   res.json(db.funds.findAll())
-})
+router.patch("/funds/:id", (req, res) => {
+  const fund = db.funds.findByIdAndUpdate(req.params.id, req.body);
+  res.json(db.funds.findAll());
+});
 
-router.delete('/funds/:id', (req, res) => {
-  let selectedFund = db.funds.find(req.params.id)
-  db.funds.delete(req.params.id)
-  res.json(selectedFund)
+router.delete("/funds/:id", (req, res) => {
+  let selectedFund = db.funds.find(req.params.id);
+  db.funds.delete(req.params.id);
+  res.json(selectedFund);
+});
 
-})
-
-
-
-module.exports = router
+module.exports = router;
